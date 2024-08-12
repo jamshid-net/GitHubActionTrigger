41db5d3dd2554848dcdabcfa2e979a304e2e0a6e
Jamshid-net <39515083+jamshid-net@users.noreply.github.com>
2024-08-13 01:43:10 +0500

    some changes

--- GitHubActionTrigger/GithubAction.cs
+++ GitHubActionTrigger/GithubAction.cs
@@ -1,5 +1,62 @@
-﻿namespace GitHubActionTrigger;
+﻿using LibGit2Sharp;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+namespace GitHubActionTrigger;
 public static class GithubAction
 {
-    //public static bool ActionTrigger(string githubToken, )
+    
+    public async static Task<bool> ActionTrigger(string githubToken, string githubRepo)
+    {
+        var url = $"https://api.github.com/repos/{githubRepo}/dispatches";
+
+        using var httpClient = new HttpClient();
+       
+        var payload = new
+        {
+            event_type = "trigger-build"
+        };
+
+        var httpMessage = new HttpRequestMessage(HttpMethod.Post, url);
+        httpMessage.Headers.Add("Authorization", $"token {githubToken}");
+        httpMessage.Headers.Add("User-Agent", "YourAppName");
+
+        var json = JsonSerializer.Serialize(payload);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+        httpMessage.Content = content;
+
+        var response = await httpClient.SendAsync(httpMessage);
+        var responseContent = await response.Content.ReadAsStringAsync();
+
+        Console.WriteLine($"Response: {responseContent}");
+
+        return response.IsSuccessStatusCode;
+    }
+
+    private async static Task<bool> LastTriggerStatus(string githubToken, string githubRepo)
+    {
+        var url = $"https://api.github.com/repos/{githubRepo}/actions/runs";
+        using var httpClient = new HttpClient();
+        var httpMessage = new HttpRequestMessage(HttpMethod.Get, url);
+        httpMessage.Headers.Add("Authorization", $"token {githubToken}");
+
+        ////to be con.........
+        ///
+        return false;
+    }
+
+}
+public class Root
+{
+    public int total_count { get; set; }
+    public List<WorkflowRun> workflow_runs { get; set; }
+}
+
+public class WorkflowRun
+{
+    public string display_title { get; set; }
+    public string status { get; set; }
+    public string conclusion { get; set; }
 }
--- GitHubActionTrigger/Program.cs
+++ GitHubActionTrigger/Program.cs
@@ -4,13 +4,13 @@ namespace GitHubActionTrigger;
 
 internal class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        var token = "github_token@";
+        var token = "token";
 
         string repoUrl = "giturl"; // Replace with your repository URL
         string localPath = @"D:\TestGitClone"; // Replace with the desired local path
-
+        await GithubAction.ActionTrigger(token, "jamshid-net/MyOila");
     }
 
    
